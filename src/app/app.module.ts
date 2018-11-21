5b730b2c6edc73ad848d4ba364ed0db1deb6b2f3
Javier <jmartin@grupoloyal.com>
2018-11-21 11:40:13 +0100

    Calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+registerLocaleData(localeEs, 'es', localeEsExtra);
+
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CommonAppModule } from './common-app/common-app.module';
@@ -14,6 +19,7 @@ import { HomeComponent } from './home/home.component';
 import { DemosComponent } from './demos/demos.component';
 import { NotificationComponent } from './notification/notification.component';
 import { DinamicoComponent } from './dinamico/dinamico.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,8 @@ import { DinamicoComponent } from './dinamico/dinamico.component';
     HomeComponent,
     DemosComponent,
     NotificationComponent,
-    DinamicoComponent
+    DinamicoComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule, FormsModule,
@@ -31,6 +38,7 @@ import { DinamicoComponent } from './dinamico/dinamico.component';
   providers: [
     LoggerService,
     { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL },
+    { provide: LOCALE_ID, useValue: 'es' },
   ],
   bootstrap: [AppComponent]
 })
