9379c0e2cd557787dbf08384fc2359ebbb4377fe
Javier <jmartin@grupoloyal.com>
2018-11-22 14:25:07 +0100

    lazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DemosComponent } from './demos/demos.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { PersonasListComponent, PersonasViewComponent, PersonasEditComponent, PersonasAddComponent } from './personas/personas.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './login/services';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full' },
@@ -12,8 +13,8 @@ const routes: Routes = [
   {path: 'demos', component: DemosComponent},
   {path: 'chisme/de/hacer/numeros', component: CalculadoraComponent},
   {path: 'personas', component: PersonasListComponent },
-  {path: 'personas/add', component: PersonasAddComponent },
-  {path: 'personas/:id/edit', component: PersonasEditComponent },
+  {path: 'personas/add', component: PersonasAddComponent, canActivate: [AuthGuard] },
+  {path: 'personas/:id/edit', component: PersonasEditComponent, canActivate: [AuthGuard] },
   {path: 'personas/:id', component: PersonasViewComponent },
   {path: 'personas/:id/:kk', component: PersonasViewComponent },
   {path: 'empleados', children: [
@@ -24,6 +25,7 @@ const routes: Routes = [
     {path: ':id/:kk', component: PersonasViewComponent },
   ]},
   {path: 'pepito/grillo', redirectTo: '/personas/2' },
+  {path: 'config', loadChildren: './config/config.module#ConfigModule' },
   {path: '404.html', component: PageNotFoundComponent},
   {path: '**', component: PageNotFoundComponent},
 ];
--- src/app/config/config.module.ts
+++ src/app/config/config.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConfigComponent } from './config/config.component';
+import { DatosComponent } from './datos/datos.component';
+import { PreferenciasComponent } from './preferencias/preferencias.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: ConfigComponent, pathMatch: 'full' },
+  {path: 'datos', component: DatosComponent},
+  {path: 'pref', component: PreferenciasComponent},
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [ConfigComponent, DatosComponent, PreferenciasComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class ConfigModule { }
